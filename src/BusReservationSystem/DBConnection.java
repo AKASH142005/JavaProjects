884b8fed70aea703da55cf740a7eaf9b193295f9
Akash <akashanbarasuz@gmail.com>
2026-06-06 21:51:48 +0530

    Create a DBConnection Class for reuse the code in the different module with minimum repetition of code

--- src/BusReservationSystem/DBConnection.java
+++ src/BusReservationSystem/DBConnection.java
@@ -2,7 +2,7 @@ package BusReservationSystem;
 
 import java.sql.*;
 
-public class DBConnection {
+public class DBConnection  {
     private final static  String url ="jdbc:mysql://localhost:3306/BusReserv";
     private final static String userName ="root";
     private final static String passWord ="ac14042005";
