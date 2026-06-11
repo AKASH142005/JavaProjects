1d66bf80dedb94b222b17472a8fd95188a88e789
Akash <akashanbarasuz@gmail.com>
2026-06-11 12:51:19 +0530

    User data access module for perform the sql operation using user details

---
+++ src/BusReservationSystem/UserService.java
@@ -0,0 +1,16 @@
+package BusReservationSystem;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+public class UserService {
+    public boolean insertUser(User newUser) throws SQLException {
+        UserDAO userDAO = new UserDAO();
+        try{
+            userDAO.insert(newUser);
+        }
+        catch (SQLIntegrityConstraintViolationException e) {
+            System.err.println("Error : The username '"+newUser.username +"' Already Exists");
+        }
+       return true;
+    }
+}
