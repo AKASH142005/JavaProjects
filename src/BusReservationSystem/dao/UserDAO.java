67a76f080c97b09b1978d1df62650c1573948764
Akash <akashanbarasuz@gmail.com>
2026-06-14 19:24:09 +0530

    userDAO manage the insert and retrieve data from the database using the prepared statement

---
+++ src/BusReservationSystem/dao/UserDAO.java
@@ -0,0 +1,41 @@
+package BusReservationSystem.dao;
+
+import BusReservationSystem.model.User;
+
+import java.sql.*;
+
+public class UserDAO {
+
+    public boolean registerUserAdding(User newUser) {
+        String query ="Insert into users(username,password_hash,full_name,role,email,phone) VALUES (?,?,?,?,?,?)";
+        try (Connection con = DBConnection.getConnection();
+        PreparedStatement pst = con.prepareStatement(query)) {
+            pst.setString(1, newUser.getUsername());
+            pst.setString(2, newUser.getPassword());
+            pst.setString(3, newUser.getFullName());
+            pst.setString(4, newUser.getRole());
+            pst.setString(5, newUser.getEmail());
+            pst.setLong(6, newUser.getPhoneNum());
+            int rows = pst.executeUpdate();
+            return rows > 0;
+        } catch (SQLException e){
+        System.out.print("DataBase Error : " + e.getMessage());
+        return false ;
+        }
+    }
+
+    public User getUserByUsername(String username){
+        String query ="select*from users where username = ?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement pst = con.prepareStatement(query)){
+              pst.setString(1 ,username);
+              ResultSet rs = pst.executeQuery();
+              if(rs.next()){
+                  User user = new User();
+              }
+        } catch ( SQLException e){
+            System.out.print("DataBase Error : " + e.getMessage());
+        }
+        return null ;
+    }
+}
