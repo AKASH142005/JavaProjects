c80a4cfbc93183b1564284f13c21143576626bd2
Akash <akashanbarasuz@gmail.com>
2026-06-14 19:18:47 +0530

    Admin menu for handle the bus crud operation

---
+++ src/BusReservationSystem/ui/AdminMenu.java
@@ -0,0 +1,12 @@
+package BusReservationSystem.ui;
+
+import BusReservationSystem.model.User;
+
+import java.util.Scanner;
+
+public class AdminMenu {
+    static Scanner input = new Scanner(System.in);
+    public void showAdminMenu(User user){
+
+    }
+}
