723c62d37c4507f9c4cdb9274bdc8148730d851e
Akash <akashanbarasuz@gmail.com>
2026-06-18 15:31:30 +0530

    add some feature in the admin module

--- src/BusReservationSystem/Mian.java
+++ src/BusReservationSystem/Mian.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 
 public class Mian {
-   public static void main() throws SQLException, ParseException {
+   public static void main() throws Exception {
         MainMenu menu = new MainMenu();
         menu.start() ;
     }
