009c05b40356be31dd0acfb2533cff5c9cd72243
Akash <akashanbarasuz@gmail.com>
2026-06-18 15:32:36 +0530

    Bus doa for handle the insertion display option

---
+++ src/BusReservationSystem/dao/BusDAO.java
@@ -0,0 +1,78 @@
+package BusReservationSystem.dao;
+
+import BusReservationSystem.model.Bus;
+import BusReservationSystem.model.Status;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusDAO {
+    public boolean insertBus(Bus bus) throws SQLException {
+        Connection con = DBConnection.getConnection();
+        String query = "INSERT INTO buses (bus_number, bus_name, bus_type, total_seats) VALUES ( ?, ?, ?, ?)";
+        PreparedStatement pst = con.prepareStatement(query);
+        pst.setInt(1,bus.getBusNo());
+        pst.setString(2,bus.getBusName());
+        pst.setString(3,bus.getBusType());
+        pst.setInt(4,bus.getTotalSeat());
+        return pst.executeUpdate() > 0;
+    }
+
+    public Bus getBusById(int id ) throws SQLException {
+        String query ="select * from buses where id = ?";
+        try(
+        Connection con = DBConnection.getConnection();
+        PreparedStatement pst = con.prepareStatement(query);){
+        pst.setInt(1,id);
+           try (ResultSet rs = pst.executeQuery();){
+               while(rs.next()){
+                   return mapRowToBus(rs);
+               }
+           }
+        }
+    return  null ;
+    }
+
+    public Bus getBusByBusNum(int busNum ) throws SQLException {
+        String query ="select * from buses where bus_number = ?";
+        try(
+                Connection con = DBConnection.getConnection();
+                PreparedStatement pst = con.prepareStatement(query);){
+            pst.setInt(1,busNum);
+            try (ResultSet rs = pst.executeQuery();){
+                while(rs.next()){
+                    return mapRowToBus(rs);
+                }
+            }
+        }
+        return  null ;
+    }
+
+
+    public List<Bus> displayAllBusDetails() throws SQLException {
+        List <Bus> buses = new ArrayList<>() ;
+        String query = "Select * from Bus";
+        try (
+        Connection con = DBConnection.getConnection();
+        Statement st = con.createStatement();){
+           try  (ResultSet rs = st.executeQuery(query);){
+               while(rs.next()){
+                   buses.add(mapRowToBus(rs));
+               }
+           }
+        }
+        return buses ;
+    }
+
+    public Bus mapRowToBus(ResultSet rs) throws SQLException {
+        Bus bus = new Bus();
+        bus.setId(rs.getInt("id"));
+        bus.setBusNo(rs.getInt("bus_number"));
+        bus.setBusName(rs.getString("bus_name"));
+        bus.setBusType(rs.getString("bus_type"));
+        bus.setStatus(Status.valueOf(rs.getString("status")));
+        bus.setTotalSeat(rs.getInt("total_seats"));
+        return bus ;
+    }
+}
