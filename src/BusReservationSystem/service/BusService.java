725b69608e5eddc8a7e7e76a6bd577402dea5fbc
Akash <akashanbarasuz@gmail.com>
2026-06-18 15:27:22 +0530

    Add service in the bus

---
+++ src/BusReservationSystem/service/BusService.java
@@ -0,0 +1,38 @@
+package BusReservationSystem.service;
+
+import BusReservationSystem.dao.BusDAO;
+import BusReservationSystem.model.Bus;
+
+import java.util.List;
+
+public class BusService {
+    private BusDAO busDAO ;
+
+    public BusService(BusDAO busDAO) {
+        this.busDAO = busDAO;
+    }
+    public BusService(){
+
+    }
+
+    public boolean addBus(Bus bus) throws Exception {
+        if(bus.getBusNo() == 0 || bus.getBusName() == null || bus.getBusName().trim().isEmpty()){
+            throw new Exception("Bus details cannot be Blank");
+        }
+        if(bus.getTotalSeat() <= 0){
+            throw new Exception("Bus total seat greater then 0");
+        }
+
+        Bus existingBus = busDAO.getBusByBusNum(bus.getBusNo());
+
+        if(existingBus != null){
+            throw new Exception("Bus with number "+bus.getBusNo()+ "is already exist");
+        }
+    return busDAO.insertBus(bus);
+    }
+    public List<Bus> ListAllBus() throws Exception{
+        return busDAO.displayAllBusDetails() ;
+    }
+
+
+}
