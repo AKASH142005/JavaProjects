c32fb25789ce19618d83fac89179db802be6b701
Akash <akashanbarasuz@gmail.com>
2026-06-26 11:31:54 +0530

    add the booking details

--- src/BusReservationSystem/model/Booking.java
+++ src/BusReservationSystem/model/Booking.java
@@ -1,27 +1,102 @@
 package BusReservationSystem.model;
-import BusReservationSystem.dao.BookingDAO;
-import BusReservationSystem.dao.BusDAO;
 
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.util.*;
-import java.text.SimpleDateFormat ;
+import java.math.BigDecimal;
+
 public class Booking {
+    private int id;
+    private String pnr;
+    private int userId;
+    private int scheduleId;
     private String passengerName ;
-    private int busNo ;
-    private Date date ;
+    private int seatNo;
+    private String bookingStatus;
+    private BigDecimal totalAmount;
 
-    public Booking() throws ParseException {
-
+    public Booking() {
     }
+
+    public Booking(int userId, int scheduleId, String passengerName, int seatNo) {
+        this.userId = userId;
+        this.scheduleId = scheduleId;
+        this.passengerName = passengerName;
+        this.seatNo = seatNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPnr() {
+        return pnr;
+    }
+
+    public void setPnr(String pnr) {
+        this.pnr = pnr;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
     public String getPassengerName (){
         return passengerName ;
     }
-    public int getBusNo(){
-        return busNo ;
-    }
-    public Date getDate(){
-        return date ;
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
     }
 
+    public int getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(int seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", pnr='" + pnr + '\'' +
+                ", userId=" + userId +
+                ", scheduleId=" + scheduleId +
+                ", passengerName='" + passengerName + '\'' +
+                ", seatNo=" + seatNo +
+                ", bookingStatus='" + bookingStatus + '\'' +
+                ", totalAmount=" + totalAmount +
+                '}';
+    }
 }
